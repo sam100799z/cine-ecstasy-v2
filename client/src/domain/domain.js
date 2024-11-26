92a5a1f439087f0b3e53040e7c865ad71b00336f
unknown <iamsam0211@gmail.com>
2024-11-26 10:44:46 +0530

    http https 2

--- client/src/domain/domain.js
+++ client/src/domain/domain.js
@@ -1,4 +1,4 @@
-// let domain = "https://cineecstasy-v2-mern.onrender.com";
-let domain = "http://localhost:3000";
+let domain = "https://cineecstasy-v2-mern.onrender.com";
+// let domain = "http://localhost:3000";
 
 export default domain;
